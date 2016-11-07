d9ca45e2e84e535dbfee360b6884e94d882ee4b6
philip Lewis <dr.p.e.lewis@gmail.com>
2016-11-07 11:48:33 +0000

    updated from getModisEE

--- setup.py
+++ setup.py
@@ -1,10 +1,32 @@
-from distutils.core import setup
+#!/usr/bin/env python
 
-setup(name='getModisEE',
-      version='1.0',
-      description='Python MODIS access from Google Earth Engine',
-      author='Philip Lewis',
-      author_email='p.lewis@ucl.ac.uk',
-      url='https://github.com/profLewis/linearBRDF',
-      packages=['getModisEE'],
-     )
+from getModisEE import getModisEE
+from getModisEE import mapsModisEE 
+
+import sys
+
+# [long,lat]
+try:
+  year = sys.argv[1]
+except:
+  year = 2005
+
+print year
+
+centre = [0.675659,52.438432]
+extent = [2.0,2.0]
+options = {'verbose':True,'centre':centre,'extent':extent,\
+           'oname':'norfolk_%4d'%int(year),'scale':500,'maxn':2000,\
+           'sensors':['MOD09GA','MYD09GA'],'recover':True,\
+           'dates':['%4d-01-01'%int(year), '%4d-12-31'%int(year)]}
+
+
+self = getModisEE(**options)
+self.maps = [self.maskEmptyPixels,\
+             self.maskClouds,\
+             self.makeBRDFKernels,\
+             self.addTime]
+
+self.get()
+self.save()
+
